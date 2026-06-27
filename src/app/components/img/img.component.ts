3f94aaf8201f4b12dd9cf1c6b6aee4c4d6e4680c
Fabian <fabianmendozaospina@outlook.com>
2026-06-27 16:33:58 -0500

    Events

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -6,10 +6,41 @@ import { Component, Input, Output, EventEmitter} from '@angular/core';
   templateUrl: './img.component.html',
   styleUrl: './img.component.scss'
 })
-export class ImgComponent {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
   @Input() img: string = "Valor inicial";
   @Output() loaded = new EventEmitter<string>();
   imageDefault = "images/default.png";
+
+  constructor() {
+    // before render
+    // no correr nada asincrono aqui (async)
+    // esto solo corre una vez
+    console.log('Constructor', 'imgValue =>', this.img);
+  }
+
+  ngOnInit() {
+    // before render
+    // si podemos correr cosas asincronas (async)
+    // corre una sola vez
+    console.log('ngOnInit', 'imgValue =>', this.img);
+  }
+
+  ngAfterViewInit() {
+    // after render
+    // handler children
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnChanges() {
+    // before and during render
+    // changes inputs -- several times
+    console.log('ngOnChanges', 'imgValue =>', this.img);
+  }
+
+  ngOnDestroy() {
+    // delete component
+    console.log('ngOnDestroy');
+  }
 
   imgError() {
     this.img = this.imageDefault;
